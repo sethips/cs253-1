28ba711c4b69daa72e748f28bfe4e4dc633454e2
Shengren Li <shengrenli12@gmail.com>
2013-03-08 01:17:26 -0800

    Done with PS2's second half.

--- shengren-cs253/ps.py
+++ shengren-cs253/ps.py
@@ -41,7 +41,118 @@ class ProblemSet2(webapp2.RequestHandler):
         t = cgi.escape(t, quote = True)
         self.writeForm(t)
 
+signup_form="""
+<h2>Signup</h2>
+<form method="post">
+  <table>
+    <tr>
+      <td class="label">
+        Username
+      </td>
+      <td>
+        <input type="text" name="username" value="%(username)s">
+      </td>
+      <td class="error">
+        %(error_username)s
+      </td>
+    </tr>
+
+    <tr>
+      <td class="label">
+        Password
+      </td>
+      <td>
+        <input type="password" name="password" value="">
+      </td>
+      <td class="error">
+        %(error_password)s
+      </td>
+    </tr>
+
+    <tr>
+      <td class="label">
+        Verify Password
+      </td>
+      <td>
+        <input type="password" name="verify" value="">
+      </td>
+      <td class="error">
+        %(error_verify)s
+      </td>
+    </tr>
+
+    <tr>
+      <td class="label">
+        Email (optional)
+      </td>
+      <td>
+        <input type="text" name="email" value="%(email)s">
+      </td>
+      <td class="error">
+        %(error_email)s
+      </td>
+    </tr>
+  </table>
+
+  <input type="submit">
+</form>
+"""
+
+import re
+USERNAME_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+EMAIL_RE    = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+
+class ProblemSet2_2(webapp2.RequestHandler):
+    def validateUsername(self, username):
+        return USERNAME_RE.match(username)
+    def validatePassword(self, password):
+        return PASSWORD_RE.match(password)
+    def validateVerify(self, password, verify):
+        return password == verify
+    def validateEmail(self, email):
+        return email == '' or EMAIL_RE.match(email)
+    def writeFormWithErrors(self, username, password, verify, email):
+        error_username = '' if self.validateUsername(username) else "That's not a valid username."
+        error_password = '' if self.validatePassword(password) else "That wasn't a valid password."
+        error_verify = ''
+        if error_password == '' and not self.validateVerify(password, verify):
+            error_verify = "Your passwords didn't match."
+        error_email = '' if self.validateEmail(email) else "That's not a valid email."
+        self.response.write(signup_form % {"username": username,
+                                           "email": email,
+                                           "error_username": error_username,
+                                           "error_password": error_password,
+                                           "error_verify": error_verify,
+                                           "error_email": error_email})
+    def get(self):
+        self.response.write(signup_form % {"username": '',
+                                           "email": '',
+                                           "error_username": '',
+                                           "error_password": '',
+                                           "error_verify": '',
+                                           "error_email": ''})
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify  = self.request.get('verify')
+        email    = self.request.get('email')
+        if self.validateUsername(username) and \
+           self.validatePassword(password) and \
+           self.validateVerify(password, verify) and \
+           self.validateEmail(email):
+            self.redirect('/ps2_2_welcome?username=%s' % username)
+        else:
+            self.writeFormWithErrors(username, password, verify, email)
+
+class ProblemSet2_2_Welcome(webapp2.RequestHandler):
+    def get(self):
+        username = self.request.get('username')
+        self.response.write('Welcome, %s!' % username)
+
 app = webapp2.WSGIApplication([('/', MainPage),
                                ('/ps1', ProblemSet1),
-                               ('/ps2', ProblemSet2)
+                               ('/ps2', ProblemSet2),
+                               ('/ps2_2', ProblemSet2_2),
+                               ('/ps2_2_welcome', ProblemSet2_2_Welcome)
                               ], debug = True)
