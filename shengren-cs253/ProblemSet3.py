6224dff566add5a9bea6b5dae5e7f08b276c9457
Shengren Li <shengrenli12@gmail.com>
2013-03-13 14:47:41 -0700

    Done with PS6. Developed based on PS3.

--- shengren-cs253/ProblemSet3.py
+++ shengren-cs253/ProblemSet3.py
@@ -1,7 +1,9 @@
 import jinja2
 import os
 import webapp2
-
+import logging
+import time
+from google.appengine.api import memcache
 from google.appengine.ext import db
 
 jinja_environment = jinja2.Environment(
@@ -13,12 +15,24 @@ class Blog(db.Model):
     content = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
+ALL_BLOGS_CACHE_KEY = 'all_blogs'
+LAST_QUERIED_CACHE_KEY = 'last_queried'
+
 class ProblemSet3(webapp2.RequestHandler):
     template = jinja_environment.get_template('ProblemSet3.html')
     def get(self):
-        blogs = db.GqlQuery('SELECT * FROM Blog ORDER BY created DESC')
+        blogs = memcache.get(ALL_BLOGS_CACHE_KEY)
+        last_queried = memcache.get(LAST_QUERIED_CACHE_KEY)
+        if blogs is None:
+            logging.error('DB QUERY')
+            blogs = db.GqlQuery('SELECT * FROM Blog ORDER BY created DESC')
+            blogs = list(blogs)
+            memcache.set(ALL_BLOGS_CACHE_KEY, blogs)
+            last_queried = time.time()
+            memcache.set(LAST_QUERIED_CACHE_KEY, last_queried)
         template_values = {
             'blogs': blogs,
+            'time_past': str(int(time.time() - float(last_queried)))
             }
         self.response.out.write(self.template.render(template_values))
 
@@ -33,6 +47,7 @@ class ProblemSet3_NewPost(webapp2.RequestHandler):
             blog = Blog(subject = blog_subject, content = blog_content)
             blog.put()
             blog_id = str(blog.key().id())
+            memcache.set(ALL_BLOGS_CACHE_KEY, None)
             self.redirect('/ps3/' + blog_id)
         else:
             template_values = {
@@ -45,11 +60,21 @@ class ProblemSet3_NewPost(webapp2.RequestHandler):
 class ProblemSet3_Post(webapp2.RequestHandler):
     template = jinja_environment.get_template('ProblemSet3_Post.html')
     def get(self, blog_id):
-        blog = Blog.get_by_id(int(blog_id))
+        blog_cache_key = 'permalink_' + blog_id
+        last_queried_cache_key = 'last_queried_' + blog_id
+        blog = memcache.get(blog_cache_key)
+        if blog is None:
+            blog = Blog.get_by_id(int(blog_id))
+            memcache.set(blog_cache_key, blog)
+            last_queried = time.time()
+            memcache.set(last_queried_cache_key, last_queried)
+        else:
+            last_queried = memcache.get(last_queried_cache_key)
         template_values = {
             'subject': blog.subject,
             'created': blog.created,
             'content': blog.content,
+            'time_past': str(int(time.time() - float(last_queried)))
             }
         self.response.out.write(self.template.render(template_values))
 
